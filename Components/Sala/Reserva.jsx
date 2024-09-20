01dfd10a5e2cd41688db0f2fb82f9dbcc3100b33
Laisadograu06 <laisaaalba@gmail.com>
2024-09-20 10:15:15 -0300

    .......

--- Components/Sala/Reserva.jsx
+++ Components/Sala/Reserva.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Reserva.css';
 
-const Reserva = ({ children, onClick }) => {
+const Reserva = ({ id, sala, status, onClick }) => {
     return (
-        <button className='botao' onClick={onClick}>
-            {children}
-        </button>
+        <>
+        
+        { status == 1 ? 
+            <button className='botao' onClick={onClick}>
+                ID: {id} - SALA: {sala} - Status: {status}
+            </button>
+        :
+            <button className='botao-off' disabled={true}>  
+                ID: {id} - SALA: {sala} - Status: {status}
+             </button>
+        }
+
+
+        </>
     );
 }
 
--- Pages/TodasPage/TodasPage.jsx
+++ Pages/TodasPage/TodasPage.jsx
@@ -1,20 +1,42 @@
 import '../TodasPage/TodasPage.css'
 import Reserva from '../../Components/Sala/Reserva'
+import { useState } from 'react'
 const  TodasPage =() => {
+
+    const [lista, setLista] = useState([
+        {"id": 1, "nome": "A1", status: 1},
+        {"id": 2, "nome": "A2", status: 0},
+        {"id": 3, "nome": "A3", status: 0},
+        {"id": 4, "nome": "A4", status: 1},
+    ])
+
+    function escolherSala(id, nome) {
+        alert("ID" + id + "Sala: " + nome)
+    }
     return(
         <div className="fundo">
             <div className="esquerda-todas">
                 <div className="cima">
             <h2> TODAS AS SALAS</h2>
                 <button className="button-todas" type="submit">Todas Salas</button>
-                <button className="button-todas" type="submit">Salas Livres</button>
                 <button className="button-todas" type="submit">Minhas Reservas</button>
             </div>
                 <div className='baixo'>
             </div>
             </div>
-            <div>
-                <Reserva />
+            <div className='salas A'>
+                {lista.length > 0 
+                &&
+                    lista.map((item) => {
+                        return(
+                            <div key={item.id}>
+                                <Reserva nome={item.nome} id={item.id} status={item.status} onClick={() => escolherSala(item.id, item.nome)}/>
+                            </div>
+                        )
+                    })
+                }
+                
+               
             </div>
         </div>
     )
